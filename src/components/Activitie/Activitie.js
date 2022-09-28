0cf708921ac5d6a39fc9b5ac1614f7cc95963698
webexpertsabbir <webexpertsabbir@gmail.com>
2022-09-29 03:52:37 +0600

    activities list

---
+++ src/components/Activitie/Activitie.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Activitie.css'
+
+const Activitie = (props) => {
+    const {name, img, description, time, age} = props.activitie;
+    return (
+        <div className='activitie-content'>
+            <img src={img} alt="" />
+            <div className='activitie-text'>
+                <h2>{name}</h2>
+                <p>{description.slice(0, 100)} ...</p>
+                <p><b>Age:</b> {age} years</p>
+                <p><b>Time required:</b> {time}s</p>
+            </div>
+            <button className='add-to-list-btn'>Add to list</button>
+        </div>
+    );
+};
+
+export default Activitie;
--- src/components/Activities/Activities.js
+++ src/components/Activities/Activities.js
@@ -1,12 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Activitie from '../Activitie/Activitie';
 import Header from '../Header/Header';
 import './Activities.css'
 
 const Activities = () => {
+
+    const [activites, setActivites] = useState([]);
+
+    useEffect( () => {
+        fetch('activites.json')
+        .then(res => res.json())
+        .then(data => setActivites(data))
+    }, [])
     return (
         <div className='activite'>
             <div>
                 <Header></Header>
+                <h3>Select today’s exercise</h3>
+                <div className='activitie-grid'>
+                    {
+                    activites.map(activitie => <Activitie
+                        key={activitie.id}
+                        activitie={activitie}
+                    ></Activitie>) 
+                    }
+                </div>
             </div>
             <div>
                 <h1>cart activites</h1>
